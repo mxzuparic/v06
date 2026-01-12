7209973af4de55b6f910cf2dc0474f22ebae714e
Melita Župarić <meli.zuparic@gmail.com>
2026-01-12 13:57:29 +0100

    Melita Župarić v07

--- app/money.h
+++ app/money.h
@@ -3,22 +3,26 @@
 #include "../MoneyUnitTest/pch.h"
 
 
-namespace vsite::oop::v7
-{
+namespace vsite {
+	namespace oop {
+		namespace v7
+		{
 
-	class money {
-		long long eur;
-		long long ct;
+			class money {
+				long long eur;
+				long long ct;
 
-		void normalize();
-	public:
-		money(long long eur = 0, long long ct = 0);
+				void normalize();
+			public:
+				money(long long eur = 0, long long ct = 0);
 
-		money& operator+=(const money& other);
-		money& operator-=(const money& other);
+				money& operator+=(const money& other);
+				money& operator-=(const money& other);
 
-		friend std::ostream& operator<<(std::ostream& os, const money& m);
-		friend std::istream& operator>>(std::istream& is, money& m);
-	};
+				friend std::ostream& operator<<(std::ostream& os, const money& m);
+				friend std::istream& operator>>(std::istream& is, money& m);
+			};
 
-}
+		}
+	}
+	}
